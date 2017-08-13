43f9ffb57491c94eabe37f6b06f3a0fcf5026afe
Mateusz Smykowski <msmykowski@interia.pl>
2017-08-13 21:36:48 +0200

    JS script

--- js/script.js
+++ js/script.js
@@ -2,10 +2,6 @@ var url = 'http://api.icndb.com/jokes/random',
     button = document.getElementById('get-joke'),
     paragraph = document.getElementById('joke');
 
-button.addEventListener('click', function () {
-    getJoke();
-});
-
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -15,5 +11,9 @@ function getJoke() {
     });
     xhr.send();
 }
+
+button.addEventListener('click', function () {
+    getJoke();
+});
 
 getJoke();
